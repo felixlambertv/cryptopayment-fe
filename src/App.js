804ab572a490349f9799e60df856226bc73d1c3c
Felix Lambert <felix@itsavirus.com>
2022-10-17 10:26:04 +0800

    fe payment

--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import { ethers } from "ethers";
+import { useState } from "react";
 
 function App() {
+  const [status, setStatus] = useState("");
+  const [uniqueCode, setUniqueCode] = useState("");
+  const [paymentAmount, setPaymentAmount] = useState("");
+
+  const be = "https://jnb4v3dr6g.execute-api.ap-southeast-1.amazonaws.com/dev";
+  // const be = "http://localhost:8080";
+
+  async function doPayment() {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    setUniqueCode(paymentAmount.substring(paymentAmount.length - 4));
+    const signer = provider.getSigner();
+
+    http.req;
+
+    const melAddress = "0x4098224Fe5343BF1702e115EDE343Cc0eFCC01D8";
+    const melAbi = [
+      "function name() view returns (string)",
+      "function symbol() view returns (string)",
+      "function balanceOf(address) view returns (uint)",
+      "function transfer(address to, uint amount)",
+      "event Transfer(address indexed from, address indexed to, uint amount)",
+    ];
+    const melContract = new ethers.Contract(melAddress, melAbi, provider);
+
+    const signerAddress = await signer.getAddress();
+    const balance = await melContract.balanceOf(signerAddress);
+    const transferAmount = ethers.BigNumber.from(paymentAmount);
+    await melContract
+      .connect(signer)
+      .transfer("0x15e976d0e97a0181964ed7b1929b83009311d7fd", transferAmount);
+  }
+
+  async function getPaymentStatus() {
+    console.log(uniqueCode);
+    const response = await axios.get(`${be}/api/v1/payment/${uniqueCode}`);
+
+    console.log(response.data);
+    setStatus(response.data.status);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="text"
+          onChange={(e) => setPaymentAmount(e.target.value)}
+        ></input>
+        <button onClick={doPayment}>TRF</button>
+        <button onClick={getPaymentStatus}>Update</button>
+        <p>{status}</p>
       </header>
     </div>
   );
